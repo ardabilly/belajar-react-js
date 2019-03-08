db3a20e76fd27c493b388089e914c701d03a7160
ardabilly <ardabilly105@gmail.com>
2019-03-08 10:48:15 +0700

    membuat todolist reactjs

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './list';
 
 
-class Toggle extends Component{
+class App extends Component {
+
   constructor(props){
     super(props)
     this.state = {
-      toggleStatus: false
+      todoItem: '',
+      items: []
     }
-
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick() {
-    this.setState(state=> ({
-      toggleStatus: !state.toggleStatus
-    }))
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      items: [...this.state.items, this.state.todoItem],
+      todoItem: ''
+    })
   }
-  
-  render(){
-    return(
-      <div>
-        <button onClick={this.handleClick}>
-        {this.state.toggleStatus ? 'ON' : 'OFF'}
-      </button>
-      <p>Kondisi sekarang {this.state.toggleStatus ? 'Menyala' : 'Mati'}</p>
 
-      </div>
-      
-    );
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value
+    })
   }
-}
 
-class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Toggle />
-        </header>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //   </header>
+      // </div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.todoItem} onChange={this.handleChange}/>
+          <button>Add</button>
+        </form>
+        <List items={this.state.items} />
       </div>
     );
   }
