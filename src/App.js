fa0848630932cd6f397932779662cfa99989477d
ardabilly <ardabilly105@gmail.com>
2019-03-11 15:33:02 +0700

    halaman SPA dan routing dengan react router dom

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import List from './list';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
+
+function home(){
+  return <h2>ini Home</h2>
+}
+function listView(){
+  return (
+    <div>
+       <h2>semua user</h2>
+       <ul>
+         <a href="user/hilman">Hilman</a> <br/>
+         <a href="user/andreas">Andreas</a>
+       </ul>
+    </div>
+  )
+}
+function noMatch(){
+  return <h2>404 jodoh not found</h2>
+}
+
+function DetailView({match}){
+  return <h2>ini halaman {match.params.name}</h2>
+}
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      items: [],
-      isLoading: true
-    }
-  }
-
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(data => this.setState({items:data, isLoading:false}))
-  }
 
   render() {
-    const {items, isLoading} = this.state
-    if(isLoading)
-    {
-      return <p>Loading....</p>
-    }
+   
     return (
-     <div>
-        <ul>
-          { items.map((item, index) => 
-            <li key={index}>{item.name}</li>
-           ) }
-        </ul>
-     </div>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <li><a href="/">Home</a></li>
+            <li><a href="/users">User</a></li>
+          </nav>
+
+          <main>
+            <Switch>
+              <Route path="/" exact component={home} />
+              <Route path="/users" exact component={listView} />
+              <Route path="/user/:name" exact component={DetailView} />
+              <Route  component={noMatch} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+     
     );
   }
 }
